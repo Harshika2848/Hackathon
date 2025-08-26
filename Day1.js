adeb9cd996101728b3e6d8d7e82071ec7137bb98
Harshika Chauhan <harshikachauhan2848@gmail.com>
2025-08-26 10:11:49 +0530

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "Harshika",
+    age: 21,
+    course: "Computer Science",
+    isEnrolled: true
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("Enrolled:", student.isEnrolled);
